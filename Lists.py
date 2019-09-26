7122115a8c13377aa42b8bf82723e59845883362
Kyle Goodwin <kyle.goodwin@kearsarge.org>
2019-09-26 10:37:29 -0400

    Updtaed lists and added getters

--- Lists.py
+++ Lists.py
@@ -1,7 +1,8 @@
-# Nintendo Character, Color, First Name letter, Number, Different Nintendo Characher, Social media platform
+# Nintendo Character, Color, Social media platform
 
 sports = ["soccer","basketball","soccer","wresting","swimming","hockey"]
 swearlist = ["poop","crap","pee","poopy","poopie","crappy"]
 colors = ["blue","green","red","orange","grey","black","cyan","teal","pink","purple","white","yellow","brown","turqoise"]
 mario_characters = ["bowser","mario","luigi","peach","wario","goomba","yoshi","toad","waluigi","bowser jr","bowser jr.","donkey kong","diddy kong","rosalina"]
 social_media_platform = ["instagram","facebook","snapchat","tumblr","whatsapp ","youtube"]
+mario_vehicle = ["kart","bike"]
