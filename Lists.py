95c1ed2e0902754fda8b7351389a94513cd1d5ff
Jack Chambers <jtcroxley@gmail.com>
2019-09-26 11:19:31 -0400

    Storys Updated

--- Lists.py
+++ Lists.py
@@ -3,6 +3,6 @@
 sports = ["soccer","basketball","wresting","swimming","hockey","football","volleyball"]
 swearlist = ["poop","crap","pee","poopy","poopie","crappy""idiot","dummy","dumb","stupid"]
 colors = ["blue","green","red","orange","grey","black","cyan","teal","pink","purple","white","yellow","brown","turqoise"]
-mario_characters = ["bowser","mario","luigi","peach","wario","goomba","yoshi","toad","waluigi","bowser jr","bowser jr.","donkey kong","diddy kong","rosalina"]
-social_media_platform = ["instagram","facebook","snapchat","tumblr","whatsapp ","youtube"]
+mario_characters = ["bowser","mario","luigi","peach","wario","goomba","yoshi","toad","waluigi","bowser jr","bowser jr.","donkey kong","diddy kong","rosalina","boo","king boo","koopa troopa","dry bones","shy guy"]
+social_media_platform = ["instagram","facebook","snapchat","tumblr","whatsapp ","youtube","twitter"]
 mario_vehicle = ["kart","bike"]
--- Story3.py
+++ Story3.py
@@ -4,14 +4,8 @@ import Getters
 def Story3(debug):
     if debug: print("Story3 Function")
     print("\n")
-    
-    friendName1 = Getters.getWord("Enter a name: ",debug)
-    sportName1 = Getters.getSport("Enter a sport: ",debug)
-    
-    output = "\n"
-    output += "One day me and my friend " + friendName1
-    output += " were playing "+sportName1+"\n"
-    output += friendName1 + " scored a point"
+    print("\n")
+    output = ""
 
-    return output 
+    return output
 
