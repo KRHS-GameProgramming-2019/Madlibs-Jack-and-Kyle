82562573e5229c8b7d5ef574e5302d61258aeaa2
Jack Chambers <jtcroxley@gmail.com>
2019-09-26 11:09:39 -0400

    Story1 Finished

--- Story1.py
+++ Story1.py
@@ -12,17 +12,18 @@ def Story1(debug=False):
     city = "Greensville"
     town = "TownTown"
     color2 = Getters.getColor("Pick a Color: ")
-    firstLetter = charater1[0].upper()
+    firstLetter = character1[0].upper()
     character2 = Getters.getMario_Character("Eneter a Mario Character: ")
-    hours = 2
+    hours = Getters.getNumber("Enter a number: ")
     socialMedia = Getters.getSocial("Enter a Social Media Platform: ")
     
     output += "Mario Kart Legacy: \n"
     output += "One day "+character1+" was walking down the street,\n"
     output += "and they saw a "+color1+" "+kartType+" and they stole it.\n"
     output += character1+" took the "+kartType+" to the "+town+" "+city+" auto shop.\n"
-    output += character1+" added a "+color2+" "+firstLetter+" to their"+kartType+".\n"
+    output += character1+" added a "+color2+" "+firstLetter+" to their "+kartType+".\n"
     output += "Now with their pimped out "+kartType+" "+character1+" went to the race track.\n"
+    output += "At the race track "+character1+" saw "+character2+" and challenged them to a race.\n"
+    output += character2+" was faster, but "+character1+" was better and won the race!"
+    
     return output
-
-print(Story1())
