86757e923f28d92de05d7d356a6c05e24ff6d005
Jack Chambers <jtcroxley@gmail.com>
2019-09-27 11:37:34 -0400

    aaaa

--- Story1.py
+++ Story1.py
@@ -9,21 +9,24 @@ def Story1(debug=False):
     character1 = Getters.getMario_Character("Enter a Mario Character: ")
     color1 = Getters.getColor("Pick a Color: ")
     kartType = Getters.getVehicle("Bike Or Kart? ")
+    word = Getters.getWord("A Noun: ")
     city = "Greensville"
     town = "TownTown"
     color2 = Getters.getColor("Pick a Color: ")
     firstLetter = character1[0].upper()
     character2 = Getters.getMario_Character("Enter a Mario Character: ",blocks=[character1.lower()])
-    hours = Getters.getNumber("Enter a number: ")
+    number1 = Getters.getNumber("Enter a number between 1 and 1000: ",1000)
+    number2 = Getters.getNumber("Enter a number between 1 and 200: ",200)
     print("\n")
     
     output += "Mario Kart Race Story: \n"
     output += "One day "+character1+" was walking down the street,\n"
     output += "and they saw a "+color1+" "+kartType+" and they stole it.\n"
     output += character1+" took the "+kartType+" to the "+town+" "+city+" auto shop.\n"
-    output += character1+" added a "+color2+" "+firstLetter+" to their "+kartType+" for "+str(hours)+" dollars.\n"
+    output += character1+" added a "+color2+" "+firstLetter+" to their "+kartType+" for "+str(number1)+" dollars.\n"
     output += "Now with their pimped out "+kartType+" "+character1+" went to the race track.\n"
     output += "At the race track "+character1+" saw "+character2+" and challenged them to a race.\n"
+    output += "The race was on the "+word1+" Cup at "+str(number2)+" CC."
     output += character2+" was faster, but "+character1+" was better and won the race!\n"
     output += "\n"
     
--- Story2.py
+++ Story2.py
@@ -4,11 +4,38 @@ import Getters
 def Story2(debug):
     if debug: print("Story2 Function")
     print("\n")
+    timeOfDay = Getters.getTOD("Enter a time of day: ")
+    character1 = Getters.getMario_Character("Enter a Mario Character: ")
+    number1 = Getters.getNumber("A number between 1 and 200: ",200)
+    number2 = Getters.getNumber("A number between 1 and 12: ",12)
+    color1 = Getters.getColor("Enter a color: ")
+    noun1 = Getters.getWord("Enter a noun: ")
+    verb1 = Getters.getVerb(endsWith="ly")
+    color2 = Getters.getColor("Enter a color: ")
+    noun2 = Getters.getWord("Enter a noun: ")
+    character2 = Getters.getMario_Character("")
+    character3 = Getters.getMario_Character("")
+    color3 = Getters.getColor("")
+    noun3 = Getters.getWord("")
+    noun4 = Getters.getWord("")
+    kartType1 = Getters.getVehicle("")
+    noun5 = Getters.getWord("")
+    
+    print("\n")
     output = ""
     output += "The Race At Bowsers Castle. \n"
     output += "\n"
-    output += "One "+timeOfDay+" "+character1+" went to a race at Bowsers Castle. \n"
-    output += "The race was on "+number+" CC"+" "
+    output += "One "+timeOfDay+" "+character1+" went to a race at Bowsers Castle.\n"
+    output += "The race was on "+number1+" CC, and the racers were ready to go!\n"
+    output += character1 + "was in the "+number2+" place to start, with their "+color1+" "+noun1+" ready to go.\n"
+    output += "They went through the course "+verb1+" and got to the first "+color2+" boost pad.\n"
+    output += character1+" hit an item box and got a "+noun2+" they immediately used it on "+character2+".\n"
+    output += "They then moved into pole position, but "+character1+" was soon contested by "+character3+" and they were fighting for the front.\n"
+    output += "The two zoomed through the "+color3+" halls and left the castle.\n"
+    output += "They both hit the "+noun3+" at the same time and cross the Finish "+noun4+".\n"
+    output += character1+" got out of their "+kartType1+" and started hitting "+character3+" with "+noun5+".\n"
+    output += character1+" lived and won the cup trophy!"
+    output += "\n"
     
 
     return output
