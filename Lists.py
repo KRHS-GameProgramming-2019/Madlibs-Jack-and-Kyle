ab61ec2021d421b6be06120e6f5ccad3d90925bf
Jack Chambers <jtcroxley@gmail.com>
2019-09-30 11:27:25 -0400

    added many bad words

--- Lists.py
+++ Lists.py
@@ -1,7 +1,8 @@
 # Nintendo Character, Color, Social media platform
+swearlist = ["poop","crap","pee","poopy","poopie","crappy","idiot","dummy","dumb","stupid","toilet","shit","nigger","bignigger","nigga","negro","slut","whore","cunt","spick","chink","mestizo","gypsie"
+,"farts","ass","asshole","asswhole","fuck","shit","shitty","shittie","shart","shitted","shat","cuck","cock","genetalia","vagina","pussy","spunk","discharge","cum","porn","Kyle Goodwin"]
 
 sports = ["soccer","basketball","wresting","swimming","hockey","football","volleyball"]
-swearlist = ["poop","crap","pee","poopy","poopie","crappy","idiot","dummy","dumb","stupid"]
 colors = ["blue","green","red","orange","grey","black","cyan","teal","pink","purple","white","yellow","brown","turqoise"]
 mario_characters = ["bowser","mario","luigi","peach","wario","goomba","yoshi","toad","waluigi","bowser jr","bowser jr.","donkey kong","diddy kong","rosalina","boo","king boo","koopa troopa","dry bones","shy guy"]
 mario_villains = ["bowser","dimentio","king boo","king-boo","wario","waluigi","bowser jr","bowser-jr.","bowser jr.","bowser-jr"]
