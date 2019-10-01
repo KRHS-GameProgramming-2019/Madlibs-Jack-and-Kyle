7956367de4fac54e1f1ef36612f510649d5cbcc5
Jack Chambers <jtcroxley@gmail.com>
2019-10-01 10:16:20 -0400

    snake egg

--- Snake.py
+++ Snake.py
@@ -1,75 +1,40 @@
-# SNAKES GAME
-# Use ARROW KEYS to play, SPACE BAR for pausing/resuming and Esc Key for exiting
+import pygame, random, sys
+from pygame.locals import *
+def collide(x1, x2, y1, y2, w1, w2, h1, h2):
+	if x1+w1>x2 and x1<x2+w2 and y1+h1>y2 and y1<y2+h2:return True
+	else:return False
+def die(screen, score):
+	f=pygame.font.SysFont('Arial', 30);t=f.render('Your score was: '+str(score), True, (0, 0, 0));screen.blit(t, (10, 270));pygame.display.update();pygame.time.wait(2000);sys.exit(0)
+xs = [290, 290, 290, 290, 290];ys = [290, 270, 250, 230, 210];dirs = 0;score = 0;applepos = (random.randint(0, 590), random.randint(0, 590));pygame.init();s=pygame.display.set_mode((600, 600));pygame.display.set_caption('Snake');appleimage = pygame.Surface((10, 10));appleimage.fill((0, 255, 0));img = pygame.Surface((20, 20));img.fill((255, 0, 0));f = pygame.font.SysFont('Arial', 20);clock = pygame.time.Clock()
+while True:
+	clock.tick(10)
+	for e in pygame.event.get():
+		if e.type == QUIT:
+			sys.exit(0)
+		elif e.type == KEYDOWN:
+			if e.key == K_UP and dirs != 0:dirs = 2
+			elif e.key == K_DOWN and dirs != 2:dirs = 0
+			elif e.key == K_LEFT and dirs != 1:dirs = 3
+			elif e.key == K_RIGHT and dirs != 3:dirs = 1
+	i = len(xs)-1
+	while i >= 2:
+		if collide(xs[0], xs[i], ys[0], ys[i], 20, 20, 20, 20):die(s, score)
+		i-= 1
+	if collide(xs[0], applepos[0], ys[0], applepos[1], 20, 10, 20, 10):score+=1;xs.append(700);ys.append(700);applepos=(random.randint(0,590),random.randint(0,590))
+	if xs[0] < 0 or xs[0] > 580 or ys[0] < 0 or ys[0] > 580: die(s, score)
+	i = len(xs)-1
+	while i >= 1:
+		xs[i] = xs[i-1];ys[i] = ys[i-1];i -= 1
+	if dirs==0:ys[0] += 20
+	elif dirs==1:xs[0] += 20
+	elif dirs==2:ys[0] -= 20
+	elif dirs==3:xs[0] -= 20	
+	s.fill((255, 255, 255))	
+	for i in range(0, len(xs)):
+		s.blit(img, (xs[i], ys[i]))
+	s.blit(appleimage, applepos);t=f.render(str(score), True, (0, 0, 0));s.blit(t, (10, 10));pygame.display.update()
+					
+					
+			
 
-import curses
-from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
-from random import randint
 
-
-curses.initscr()
-win = curses.newwin(20, 60, 0, 0)
-win.keypad(1)
-curses.noecho()
-curses.curs_set(0)
-win.border(0)
-win.nodelay(1)
-
-key = KEY_RIGHT                                                    # Initializing values
-score = 0
-
-snake = [[4,10], [4,9], [4,8]]                                     # Initial snake co-ordinates
-food = [10,20]                                                     # First food co-ordinates
-
-win.addch(food[0], food[1], '*')                                   # Prints the food
-
-while key != 27:                                                   # While Esc key is not pressed
-    win.border(0)
-    win.addstr(0, 2, 'Score : ' + str(score) + ' ')                # Printing 'Score' and
-    win.addstr(0, 27, ' SNAKE ')                                   # 'SNAKE' strings
-    win.timeout(150 - (len(snake)/5 + len(snake)/10)%120)          # Increases the speed of Snake as its length increases
-    
-    prevKey = key                                                  # Previous key pressed
-    event = win.getch()
-    key = key if event == -1 else event 
-
-
-    if key == ord(' '):                                            # If SPACE BAR is pressed, wait for another
-        key = -1                                                   # one (Pause/Resume)
-        while key != ord(' '):
-            key = win.getch()
-        key = prevKey
-        continue
-
-    if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:     # If an invalid key is pressed
-        key = prevKey
-
-    # Calculates the new coordinates of the head of the snake. NOTE: len(snake) increases.
-    # This is taken care of later at [1].
-    snake.insert(0, [snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1), snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
-
-    # If snake crosses the boundaries, make it enter from the other side
-    if snake[0][0] == 0: snake[0][0] = 18
-    if snake[0][1] == 0: snake[0][1] = 58
-    if snake[0][0] == 19: snake[0][0] = 1
-    if snake[0][1] == 59: snake[0][1] = 1
-
-    # Exit if snake crosses the boundaries (Uncomment to enable)
-    #if snake[0][0] == 0 or snake[0][0] == 19 or snake[0][1] == 0 or snake[0][1] == 59: break
-
-    # If snake runs over itself
-    if snake[0] in snake[1:]: break
-
-    
-    if snake[0] == food:                                            # When snake eats the food
-        food = []
-        score += 1
-        while food == []:
-            food = [randint(1, 18), randint(1, 58)]                 # Calculating next food's coordinates
-            if food in snake: food = []
-        win.addch(food[0], food[1], '*')
-    else:    
-        last = snake.pop()                                          # [1] If it does not eat the food, length decreases
-        win.addch(last[0], last[1], ' ')
-    win.addch(snake[0][0], snake[0][1], '#')
-    
-curses.endwin()
